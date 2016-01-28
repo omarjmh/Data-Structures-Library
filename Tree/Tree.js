ecacbb52163bea982709a8bb6acb804602b3df99
Omar Sial <omar.sial1@gmail.com>
2016-01-27 22:16:36 -0500

    adding to Tree

--- Tree/Tree.js
+++ Tree/Tree.js
@@ -9,8 +9,8 @@ class Tree {
     this.children.push(node);
   }
   DFselect(callback) {
-    callback(this.value);
-    this.children.forEach(function(child) {
+    callback(this.value, this.children);
+    this.children.forEach(function(child, i) {
       // callback(child.value);
       child.DFselect(callback);
     });
@@ -23,9 +23,52 @@ class Tree {
       currentNode.children.forEach(function(child) {
         queue.push(child);
       })
-      callback(currentNode.value);
+      callback(currentNode.value, currentNode.children);
       currentNode = queue.shift();
     }
+  }
+  mapDF(callback) {
+    let mapped = [];
+    this.DFselect(a => mapped.push(callback(a)));
+    return mapped;
+  }
+  mapBF(callback) {
+    let mapped = [];
+    this.BFselect(a => mapped.push(callback(a)));
+    return mapped;
+  }
+  filter(callback) {
+    let filtered = [];
+    this.DFselect(a => {
+      if (callback(a)) {
+        filtered.push(callback(a));
+      }
+    });
+    return filtered;
+  }
+  contains(value) {
+    return this.filter(a => a === value).length > 0;
+  }
+  isDescendant(child) {
+    let result = false;
+    this.DFselect(a => {
+      if (a === child) {
+        result = true;
+        return;
+      }
+    });
+    return result;
+  }
+  remove(value) {
+    var self = this;
+    (function recurse(currentNode, parent) {
+      currentNode.children.forEach((a, i) => {
+        if (a.value === value) {
+          self.children[i] = null;
+          return;
+        }
+      })
+    })(this);
   }
 }
 
