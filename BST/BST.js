9094f94e474de451dcbfc66af9529e665c3462a8
Omar Sial <omar.sial1@gmail.com>
2016-01-28 00:07:18 -0500

    BST to ES6

--- BST/BST.js
+++ BST/BST.js
@@ -1,51 +1,43 @@
-var Node = function(value) {
-  this.value = value;
-  this.right = null;
-  this.left = null;
-};
-var BinarySearchTree = function(value) {
-  this._root = new Node(value);
-};
-BinarySearchTree.prototype.add = function(value) {
-  var node = new Node(value);
-  if (this._root === null) {
-    this._root = node;
-  } else {
-      var insertKey;
-      var current = this._root;
-      insertKey = current.value > value ? 'left' : 'right';
-      while (current) {
-       if (!current[insertKey]) {
-         current[insertKey] = node;
-         return;
-       } else {
-         current = current[insertKey];
-       }
-      }
-    }
-};
-BinarySearchTree.prototype.postOrder = function(current, callback) {
-  if (!current) {
-    return;
+"use strict"
+class Node {
+  constructor(value) {
+      this.value = value;
+      this.right = null;
+      this.left = null;
   }
-  this.postOrder(current.left, callback);
-  this.postOrder(current.right, callback);
-  if (callback.constructor === Function) {
-    callback(current);
-  }
-};
-
-var l = console.log;
-var t = new BinarySearchTree(50);
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
 }
-for (var i = 0; i < 25; i++) {
-  t.add(getRandomInt(1,45))
-};
 
+class BinarySearchTree {
+  constructor(value) {
+      this._root = new Node(value);
+  }
 
-t.postOrder(t, function(a) {l(a)})
-
-
-
+  add(value) {
+    let node = new Node(value);
+    if (this._root === null) {
+        this._root = node;
+    } else {
+        let insertKey;
+        let current = this._root;
+        insertKey = current.value > value ? 'left' : 'right';
+        while (current) {
+          if (!current[insertKey]) {
+            current[insertKey] = node;
+            return;
+          } else {
+              current = current[insertKey];
+            }
+        }
+      }
+  };
+  postOrder(current, callback) {
+    if (!current) {
+        return;
+    }
+    this.postOrder(current.left, callback);
+    this.postOrder(current.right, callback);
+    if (callback.constructor === Function) {
+        callback(current);
+    }
+  }
+}
